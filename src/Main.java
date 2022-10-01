cbef12cdb8bf8f1a78e10e187e7f7c3d86a602e1
fhvfn94 <fhvfn94@gmail.com>
2022-10-01 13:59:33 +0300

    add task 3

--- src/Main.java
+++ src/Main.java
@@ -2,6 +2,7 @@ public class Main {
     public static void main(String[] args) {
         task1();
         task2();
+        task3();
     }
     public static void task1(){
         byte age = 18;
@@ -19,6 +20,24 @@ public class Main {
             System.out.println("человек уже закончил школу и может отправляться в университет, если его возраст 18 и больше;");
         } else if (age >= 24) {
             System.out.println("человек окончил университет и ему пора искать первую работу, если ему 24 и больше лет.");
+        }
+    }
+    public static void task3(){
+        byte train = 102;
+        byte seatPlace = 60;
+        int stayPlace = train - seatPlace;
+        byte people = 100;
+        if (train > people){
+            if (seatPlace > people) {
+                seatPlace -= people;
+                System.out.println(seatPlace + " сидячих мест и " + stayPlace + "стоячих мест");
+            } else if (seatPlace < people) {
+                people -= seatPlace;
+                stayPlace -= people;
+                System.out.println("0 сидячих мест и " + stayPlace + " стоячих мест");
+            }
+        } else {
+            System.out.println("Нет свободных мест");
         }
     }
 }
